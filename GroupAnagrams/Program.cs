f5dfcd7fbb9902334b5c720b1dec0b0ab117e0ad
Marco Fortes <marco.aurelio@ambipar.com>
2025-02-05 15:40:22 -0300

    Exercises

---
+++ GroupAnagrams/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace GroupAnagrams
+{
+    internal class Program
+    {
+        static List<List<string>> GroupAnagrams(List<string> strings)
+        {
+            Dictionary<string, List<string>> storedAnagrams = new();
+            foreach (var word in strings)
+            {
+                string sortedW = String.Concat(word.OrderBy(c => c));
+                if (!storedAnagrams.ContainsKey(sortedW))
+                    storedAnagrams.Add(sortedW, new());
+                storedAnagrams[sortedW].Add(word);
+            }
+            return storedAnagrams.Values.ToList();
+        }
+        static void Main(string[] args)
+        {
+            var input1 = new List<string>() { "eat", "tea", "tan", "ate", "nat", "bat" };
+            var grouped = GroupAnagrams(input1);
+        }
+    }
+}
